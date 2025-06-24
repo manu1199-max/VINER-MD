f1c8ef7a9d17bf873775d533ce2b713fbacb9891
lg221 <liverboy233@gmail.com>
2025-06-24 09:07:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "MD=JgZxBa5B#zdzB0Ul5p2ltQ47ACHKpG3a4o7vdCvImv2h_t9j712s", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // ඔයාගේ mongodb url එක දාන්න
 BOT_NAME: process.env.BOT_NAME || "VINER-MD",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rdl1p6.jpg",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
