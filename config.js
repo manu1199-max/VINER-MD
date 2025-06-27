6e8973ab1928342935245cc2e86edfdfbe5745c3
manu1199-max <manusenavirathna4@gmail.com>
2025-06-27 20:27:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MD=JgZxBa5B#zdzB0Ul5p2ltQ47ACHKpG3a4o7vdCvImv2h_t9j712s", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "MD=txBy1Jwa#2_JjPMExrlVliA5bHorX4bJh63f2aptxjI2ZkRtHmxc", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // ඔයාගේ mongodb url එක දාන්න
 BOT_NAME: process.env.BOT_NAME || "VINER-MD",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rdl1p6.jpg",
